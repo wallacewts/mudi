a20d6901f365f206ec30b8e90f78cb44ed0441a4
Matheus Wallace <matheuswts2011@hotmail.com>
2022-05-24 08:21:51 -0300

    add entity notation for pedido model

--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -3,13 +3,31 @@ package br.com.alura.mvc.mudi.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pedido {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
   private String nomeProduto;
   private BigDecimal valorNegociado;
   private LocalDate dataEntrega;
   private String urlProduto;
   private String urlImagem;
   private String descricao;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
 
   public String getNomeProduto() {
     return nomeProduto;
