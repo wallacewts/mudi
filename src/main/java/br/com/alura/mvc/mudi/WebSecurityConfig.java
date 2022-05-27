9be02fc7c762cbf920416104d290de518674d1c5
Matheus Wallace <matheuswts2011@hotmail.com>
2022-05-27 10:40:00 -0300

    disable spring csrf

--- src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -29,7 +29,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.defaultSuccessUrl("/home", true)
 				.permitAll()
 			)
-			.logout(logout -> logout.logoutUrl("/logout"));
+			.logout(logout -> logout.logoutUrl("/logout"))
+			.csrf().disable();
 	}
 
 	@Override
