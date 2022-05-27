0102627c088d7c672bbded560d184887321eeef3
Matheus Wallace <matheuswts2011@hotmail.com>
2022-05-27 11:42:33 -0300

    enable static pagination and sort

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,14 +1,14 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
@@ -24,7 +24,9 @@ public class HomeController {
 
   @GetMapping
   public String home(Model model) {
-    List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE);
+    Sort sortByDataEntrega = Sort.by("dataEntrega").descending();
+    PageRequest paginacao = PageRequest.of(0, 10, sortByDataEntrega);
+    List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE, paginacao);
     model.addAttribute("pedidos", pedidos);
 
     return "home";
